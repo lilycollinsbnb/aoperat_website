fe82e8d584ccad22ffc648f2cd8db0034bb0f39d
Marcin Flądro <marcin.fladro@matrix-reliability.eu>
2022-06-23 14:19:16 +0200

    initial commit and color change

---
+++ src/templates/faq-page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from "../components/Layout";
+
+export default function FaqPage() {
+  return (
+    <Layout>
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2>FAQ</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    </Layout>
+  )
+}
