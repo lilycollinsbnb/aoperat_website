9c5b389ee4ae1c7d10b5abb9c6922ac27fe71110
Marcin Flądro <marcin.fladro@matrix-reliability.eu>
2022-10-04 17:00:12 +0200

    Aoperat -> AOperat

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Aoperat",
+    title: "AOperat",
     description:
       "",
   },
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Aoperat"
+            alt="AOperat"
             style={{ width: "14em", height: "10em" }}
           />
         </div>
