889f3e6fadbe187485496224dfa2cb7467ab1044
BorysBe <b.bobulski@wp.pl>
2023-03-12 14:44:25 +0100

    - add trainings page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -47,6 +47,11 @@ const Footer = class extends React.Component {
                     </Link>
                   </li>
                   <li>
+                    <Link className="navbar-item" to="/trainings">
+                      Szkolenia
+                    </Link>
+                  </li>
+                  <li>
                     <Link className="navbar-item" to="/contact">
                       Kontakt
                     </Link>
