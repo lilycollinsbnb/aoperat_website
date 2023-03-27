cb12e0dc9e46ef624d9aafbfc0e313f1f41e8974
Marcin Flądro <marcin.fladro@matrix-reliability.eu>
2023-03-27 16:42:49 +0200

    google analytics and cookie consent

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,14 @@ import * as React from "react";
 import { Helmet } from "react-helmet";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./all.sass";
 import "./a_styles.sass";
 import useSiteMetadata from "./SiteMetadata";
 import { withPrefix } from "gatsby";
 import { Link } from "gatsby";
 import logoHor from "../img/logo-hor.svg";
+import {CookieNotice} from "gatsby-cookie-notice";
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata();
@@ -60,6 +62,20 @@ const TemplateWrapper = ({ children }) => {
           </Link>
         </div>
       </section>
+      <CookieNotice
+        backgroundClasses="container is-background-secondary-dark"
+        acceptButtonText="Akceptuję"
+        declineButtonText="Odrzucam"
+        personalizeButtonText="Personalizuj"
+        personalizeValidationText="Zapisz"
+        cookies={[
+                {name: 'necessary', editable: false, default: true, title: 'Niezbędne pliki cookie', text: 'Pliki niezbędne do poprawnego funkcjonowania strony.' },
+                {name: 'gatsby-gdpr-google-analytics', editable: true, default: false, title: 'Google Analytics', text: 'Google Analytics to narzędzie statystyczne stworzone przez Google pozwalające na analizę ruchu sieciowego i sposobu korzystania z naszej strony.'},
+                {name: 'gatsby-gdpr-linked-in', editable: true, default: false, title: 'LinkedIn', text: 'LinkedIn Insight Tag pozwala nam na skuteczniej dotrzeć do potencjalnych klientów.'}
+          ]}>
+          <h4 className="is-color-primary-green">Ta strona korzysta z plików cookies.</h4>
+          <p className="is-color-white">Ty wybierasz, z których plików cookie będziemy mogli korzystać. Korzystając z serwisu zgadzasz się na ich zapis i wykorzystanie plików cookie oraz akceptujesz politykę plików cookie.</p>
+      </CookieNotice>
       <div>{children}</div>
       <Footer />
     </div>
