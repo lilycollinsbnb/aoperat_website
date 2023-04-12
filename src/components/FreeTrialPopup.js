f532cf6d812025815d9f78906a8bad2915dc3326
Marcin Flądro <marcin.fladro@matrix-reliability.eu>
2023-04-12 11:24:36 +0200

    font and logo placement

--- src/components/FreeTrialPopup.js
+++ src/components/FreeTrialPopup.js
@@ -10,8 +10,7 @@ export default function FreeTrialPopup () {
 
     return (
         <Popup 
-            className="popup-bg"
-            contentStyle={{minWidth: "300px", maxHeight: "600px", overflow: "auto", borderRadius: "15px", zIndex: "200000"}} 
+            contentStyle={{minWidth: "300px", maxHeight: "600px", overflow: "auto", borderRadius: "15px", zIndex: "200000", padding: "0px"}} 
             overlayStyle={{zIndex: "20000"}}
             defaultOpen={true} 
             modal
@@ -19,15 +18,14 @@ export default function FreeTrialPopup () {
         >
       { close => (
         <div className="popup-content-div">
-          <img height={"100px"} width={"100px"} src="../img/logo.svg" alt="" />
-          <button type="button" style={{gridColumn:"5/5", backgroundColor: "transparent", border: "none", outline: "none", fontSize: "30px"  }} onClick={close} >
+          <div className="logo-wrapper">
+            <img className="popup-logo mt-3" src="../img/logo.svg" alt="" />
+          </div>
+          <button className="popup-close-button" type="button"  onClick={close} >
             &times;
           </button>
-          <div style={{padding: "20px 0px"}}>
-            <div className="" > 
-              <h3 style={{textAlign: "center"}} className="has-text-weight-semibold is-size-5-mobile is-size-4-tablet is-size-4-widescreen is-color-secondary-dark mt-3">AOPERAT.PL</h3>
-            </div>
-            <div className="center-content mt-5">
+          <div className="popup-form-wrapper">
+            <div className="center-content is-medium-large-font mt-5">
               Chesz uzyskać możliwość <b>darmowego</b> używania platformy AOperat przez <b>3 miesiące</b>? Zostaw swój adres mailowy.
             </div>
             <div className="center-content mt-3">
@@ -59,11 +57,11 @@ export default function FreeTrialPopup () {
                       required={true}
                       
                     />
-                  <label htmlFor="consent-to-contact" className="center-content">Na podstawie art. 10 ust. 2 ustawy o świadczeniu usług drogą elektroniczną wyrażam zgodę na przekazywanie przez AOperat Prosta Spółka Akcyjna, na udostępniony przeze mnie adres e-mail informacji handlowych.</label>
+                  <label htmlFor="consent-to-contact" className="center-content is-small-font-2">Na podstawie art. 10 ust. 2 ustawy o świadczeniu usług drogą elektroniczną wyrażam zgodę na przekazywanie przez AOperat Prosta Spółka Akcyjna, na udostępniony przeze mnie adres e-mail informacji handlowych.</label>
                 </div>
                 <button type="submit" style={{width: "100%"}} className="mt-5 has-text-weight-bold button mrb-button mrb-button-dark">ZAPISZ MNIE</button>
                 
-                <div className="mrb-text-div mt-6">
+                <div className="mrb-text-div is-small-font mt-3 mb-3">
                   Przed wyrażeniem zgody prosimy o zapoznanie się z <Link to="/terms-and-conditions">Regulaminem</Link> i <Link to="/privacy-policy">Polityką Prywantości</Link>. Wyrażenie zgody na kontakt drogą mailową i zaakceptowanie postanowień Polityki Prywatności oraz Regulaminu jest dobrowolne ale niezbędne do tego abyśmy mogli się z Tobą skontaktować. Zgodę można w każdej chwili cofnąć kontaktując się z Administratorem Danych Osobwych wskazanym w <Link to="/privacy-policy">Polityce Prywantości</Link>. 
                 </div>
                
