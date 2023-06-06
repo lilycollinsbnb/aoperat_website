37e52127d4332a990f077101c3c523d8e6f64c2f
Marcin Flądro <marcin.fladro@matrix-reliability.eu>
2023-06-06 16:02:22 +0200

    404 update

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ const NotFoundPage = () => (
     <section>
       <div className="container is-max-desktop mrb-container">
         <h1 className="has-text-weight-bold has-text-centered is-size-3-mobile is-size-2-tablet is-size-1-widescreen is-color-primary-green">
-          Błąd 404 - nie znaleziono
+          Błąd 404 - Nie znaleziono
         </h1>
         <p className="has-text-centered" style={{fontSize:"16px", marginTop:"10px"}}>Podstrona nie istnieje lub została usunięta.</p>
       </div>
